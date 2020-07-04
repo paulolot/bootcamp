cc178278dddcb871be00ef780f8b2fd56e3ce175
paulolot <paulo.a.lot@gmail.com>
2020-07-04 09:26:28 -0300

    INICIAL

---
+++ index.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+mongoose
+  .connect(
+    //'mongodb+srv://paulo:123456@bootcamp.lzptq.mongodb.net/teste?retryWrites=true&w=majority',
+    'mongodb+srv://paulolot:pLqb4n6egiysShnd@bootcamp.lzptq.mongodb.net/trabalhoPratico?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(console.log('db conectado'))
+  .catch((err) => {
+    console.log('erro ao conectar ' + err);
+  });
